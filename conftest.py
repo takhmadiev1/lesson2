9e0aee3fb7ceec8923c749582d892346c5ab8bdb
takhmadiev <davidovskiy88@gmail.com>
2023-09-20 11:33:41 +0500

    Fixing fixtures,delete random size of browser window

--- conftest.py
+++ conftest.py
@@ -1,21 +1,13 @@
 import pytest
 from selene.support.shared import browser
-import random
 
 
 @pytest.fixture
-def resize_browser_height():
-    browser.config.window_height = random.randint(500, 1000)
+def resize_browser():
+    browser.config.window_height = 1000
+    browser.config.window_width = 1000
 
     yield
 
     browser.quit()
 
-
-@pytest.fixture
-def resize_browser_width():
-    browser.config.window_width = random.randint(1000, 2000)
-
-    yield
-
-    browser.quit()
--- test_google_search.py
+++ test_google_search.py
@@ -2,13 +2,13 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-def test_valid_search_yashaka(resize_browser_height, resize_browser_width):
+def test_valid_search_yashaka(resize_browser):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
 
 
-def test_found_nothing(resize_browser_height, resize_browser_width):
+def test_found_nothing(resize_browser):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('hvkjhdcnckjsdncvdcevvsdchgvsdc').press_enter()
     browser.element('.card-section').should(have.text('По запросу hvkjhdcnckjsdncvdcevvsdchgvsdc ничего не найдено.'))
